55e14ff6d2aac1844cce59669ca4365bd7353778
Deymeqpyra <deymecop@gmail.com>
2024-10-29 15:02:21 +0200

    feat: configured the repositories

--- ProjectManagment/Infrastructure/Persistence/ConfigurePersistence.cs
+++ ProjectManagment/Infrastructure/Persistence/ConfigurePersistence.cs
@@ -1,3 +1,6 @@
+using Application.Common.Interfaces.Queries;
+using Application.Common.Interfaces.Repositories;
+using Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
@@ -23,5 +26,49 @@ public static class ConfigurePersistence
                 .ConfigureWarnings(w=>w.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning)));
 
         services.AddScoped<ApplicationDbContextInitializer>();
+        services.AddRepositories();
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<UserRepository>();
+        services.AddScoped<IUserRepository>(provider => provider.GetRequiredService<UserRepository>());
+        services.AddScoped<IUserQueries>(provider => provider.GetRequiredService<UserRepository>());
+        
+        services.AddScoped<ProjectRepository>();
+        services.AddScoped<IProjectRepository>(provider => provider.GetRequiredService<ProjectRepository>());
+        services.AddScoped<IProjectQueries>(provider => provider.GetRequiredService<ProjectRepository>());
+        
+        services.AddScoped<TagRepository>();
+        services.AddScoped<ITagRepository>(provider => provider.GetRequiredService<TagRepository>());
+        services.AddScoped<ITagQueries>(provider => provider.GetRequiredService<TagRepository>());
+        
+        services.AddScoped<TagProjectRepository>();
+        services.AddScoped<ITagProjectRepository>(provider => provider.GetRequiredService<TagProjectRepository>());
+        services.AddScoped<ITagProjectQueries>(provider => provider.GetRequiredService<TagProjectRepository>());
+        
+        services.AddScoped<CategoryRepository>();
+        services.AddScoped<ICategoryRepository>(provider => provider.GetRequiredService<CategoryRepository>());
+        services.AddScoped<ICategoryQueries>(provider => provider.GetRequiredService<CategoryRepository>());
+        
+        services.AddScoped<PriorityRepository>();
+        services.AddScoped<IPriorityRepository>(provider => provider.GetRequiredService<PriorityRepository>());
+        services.AddScoped<IPriorityQueries>(provider => provider.GetRequiredService<PriorityRepository>());
+        
+        services.AddScoped<StatusRepository>();
+        services.AddScoped<IStatusRepository>(provider => provider.GetRequiredService<StatusRepository>());
+        services.AddScoped<IStatusQueries>(provider => provider.GetRequiredService<StatusRepository>());
+        
+        services.AddScoped<ProjectUserRepository>();
+        services.AddScoped<IProjectUserRepository>(provider => provider.GetRequiredService<ProjectUserRepository>());
+        services.AddScoped<IProjectUserQueries>(provider => provider.GetRequiredService<ProjectUserRepository>());
+        
+        services.AddScoped<RoleRepository>();
+        services.AddScoped<IRoleRepository>(provider => provider.GetRequiredService<RoleRepository>());
+        services.AddScoped<IRoleQueries>(provider => provider.GetRequiredService<RoleRepository>());
+        
+        services.AddScoped<TaskRepository>();
+        services.AddScoped<ITaskRepository>(provider => provider.GetRequiredService<TaskRepository>());
+        services.AddScoped<ITaskQueries>(provider => provider.GetRequiredService<TaskRepository>());
     }
 }
