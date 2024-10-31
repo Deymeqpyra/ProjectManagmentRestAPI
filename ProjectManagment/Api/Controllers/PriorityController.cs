d91beae8ce6c8e8ba515362b9dfd94d8c70e595d
Deymeqpyra <deymecop@gmail.com>
2024-10-31 16:58:23 +0200

    feat: finish controller for priority

--- ProjectManagment/Api/Controllers/PriorityController.cs
+++ ProjectManagment/Api/Controllers/PriorityController.cs
@@ -2,6 +2,7 @@ using Api.Dtos;
 using Api.Modules.Errors;
 using Application.Common.Interfaces.Queries;
 using Application.Priorities.Commands;
+using Domain.Priorities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,8 +16,19 @@ public class PriorityController(ISender sender, IPriorityQueries priorityQueries
     public async Task<ActionResult<IReadOnlyList<PriorityDto>>> GetAllPriority(CancellationToken cancellationToken)
     {
         var entities = await priorityQueries.GetAll(cancellationToken);
-        
+
         return entities.Select(PriorityDto.FromDomainModel).ToList();
+    }
+    [HttpGet("GetById/{priorityId:guid}")]
+    public async Task<ActionResult<PriorityDto>> GetById([FromRoute] Guid priorityId, CancellationToken cancellationToken)
+    {
+
+        var entity = await priorityQueries.GetById(new ProjectPriorityId(priorityId), cancellationToken);
+
+        return entity.Match<ActionResult<PriorityDto>>(
+            p => PriorityDto.FromDomainModel(p),
+            () => NotFound()
+        );
     }
     [HttpPost("CreatePriority")]
     public async Task<ActionResult<PriorityDto>> Create([FromBody] string Name, CancellationToken cancellationToken)
@@ -25,11 +37,47 @@ public class PriorityController(ISender sender, IPriorityQueries priorityQueries
         {
             Name = Name
         };
-        
+
         var result = await sender.Send(input, cancellationToken);
         return result.Match<ActionResult<PriorityDto>>(
-            p=>PriorityDto.FromDomainModel(p),
-            e=>e.ToObjectResult());
+            p => PriorityDto.FromDomainModel(p),
+            e => e.ToObjectResult());
     }
-    
+
+    [HttpPut("UpdatePriority/{priorityId:guid}")]
+    public async Task<ActionResult<PriorityDto>> Update(
+        [FromRoute] Guid priorityId,
+        [FromBody] string Name,
+        CancellationToken cancellationToken)
+    {
+        var input = new UpdatePriorityCommand
+        {
+            PriorityId = priorityId,
+            UpdateName = Name
+        };
+
+        var result = await sender.Send(input, cancellationToken);
+
+        return result.Match<ActionResult<PriorityDto>>
+        (
+            p => PriorityDto.FromDomainModel(p),
+            e => e.ToObjectResult()
+        );
+    }
+
+    [HttpDelete("DeletePriority/{priorityId:guid}")]
+    public async Task<ActionResult<PriorityDto>> Delete([FromRoute] Guid priorityId, CancellationToken cancellationToken)
+    {
+        var input = new DeletePriorityCommand
+        {
+            PriorityId = priorityId
+        };
+
+        var result = await sender.Send(input, cancellationToken);
+
+        return result.Match<ActionResult<PriorityDto>>(
+            p => PriorityDto.FromDomainModel(p),
+            e => e.ToObjectResult()
+        );
+    }
 }
--- ProjectManagment/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ ProjectManagment/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3563000caf6f6df59bd748379967e2a6212aab8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c21eca4b973a1e80c07316482e902d4214b1ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
